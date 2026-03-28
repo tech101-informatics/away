c81661e1c653182103ac2a330e395b263f3886ff
Raison Joseph <raisonjosephau@gmail.com>
2026-03-28 15:05:48 +0530

    fix spacing issue

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -38,14 +38,14 @@ CardDescription.displayName = "CardDescription";
 
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn("p-4 md:p-6 pt-0", className)} {...props} />
+    <div ref={ref} className={cn("p-4 md:p-6 !pt-0", className)} {...props} />
   )
 );
 CardContent.displayName = "CardContent";
 
 const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn("flex items-center p-4 md:p-6 pt-0", className)} {...props} />
+    <div ref={ref} className={cn("flex items-center p-4 md:p-6 !pt-0", className)} {...props} />
   )
 );
 CardFooter.displayName = "CardFooter";
